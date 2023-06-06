3c2535074a0e0f762837e23544ee3552ed8dd703
Shakil57375 <shakil57375@gmail.com>
2023-06-06 23:58:19 +0600

    footer added

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const Header = () => {
     const items = <div className="flex flex-col lg:flex-row gap-5">
         <Link className="text-lg text-gray-700 font-Poppins" to="/">Home</Link>
-        <Link className="text-lg text-gray-700 font-Poppins" to="/allToys">Instructors</Link>
-        <Link className="text-lg text-gray-700 font-Poppins" to="/blogs">Classes</Link>
-        <Link className="text-lg text-gray-700 font-Poppins" to="/blogs">Dashboard</Link>
+        <Link className="text-lg text-gray-700 font-Poppins" to="/instructors">Instructors</Link>
+        <Link className="text-lg text-gray-700 font-Poppins" to="/classes">Classes</Link>
+        <Link className="text-lg text-gray-700 font-Poppins" to="/dashboard">Dashboard</Link>
     </div>
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-xl py-8 px-8">
+      <div className="navbar bg-base-100 shadow-xl lg:py-8 py-4 lg:px-8 px-4">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -30,13 +30,13 @@ const Header = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-compact dropdown-content mt-2 pl-5 py-3 shadow bg-base-100  w-52 "
             >
               {items}
             </ul>
           </div>
           <Link to="/" className="btn btn-ghost normal-case text-xl">
-            <img src="https://i.ibb.co/GTYH3pj/sports-academy.png" className="w-28 h-[70px]" alt="" />
+            <img src="https://i.ibb.co/GTYH3pj/sports-academy.png" className="lg:w-28 lg:h-16 h-12 w-20 ml-12 lg:ml-0" alt="" />
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
@@ -46,7 +46,7 @@ const Header = () => {
         </div>
         <div className="navbar-end">
           <Link to="/login">
-          <button className="btn">Login</button>
+          <button className="btn my-btn">Login</button>
           </Link>
         </div>
       </div>
